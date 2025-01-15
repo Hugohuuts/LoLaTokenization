a66f474ab33b77409e66d9304c142a14e1376654
6950167 <60781435+6950167@users.noreply.github.com>
2025-01-15 18:51:10 +0100

    Update custom tokenizers to include separators

--- tokenizers_pos.py
+++ tokenizers_pos.py
@@ -5,59 +5,63 @@ NLP = spacy.load("en_core_web_sm")
 nltk.download('words')
 english_words = set(words.words())
 
-def split_noun_hybrid(noun):
+def split_word_hybrid(word):
     """
-    Split nouns using an english lexicon
+    Function to tokenize a single word from a sentence. Called by pos_tokenizer
+    ---
+    Args:
+        word: The word in string format
+    Returns:
+        A string tokenized by adding spaces between components
     """
     # Step 1: Try dictionary-based splitting
-    components = []
-    for i in range(1, len(noun)):
-        prefix = noun[:i]
-        suffix = noun[i:]
+    components = ""
+    for i in range(1, len(word)):
+        prefix = word[:i]
+        suffix = word[i:]
         if prefix in english_words and suffix in english_words:
-            components.extend([prefix, suffix])
+            components += f"{prefix} {suffix}"
             break
-    #else:
+    else:
         # Step 2: Fallback to subword splitting (e.g., character bigrams)
-        #components = list(noun)
+        components += ' '.join(word)
     
-    return components if components else [noun]
+    return components
 
 
-def pos_tokenizer(premises, hypothesis, tags):
+def pos_tokenizer(sentence, tags, separator):
     """
-    Tokenizer that splits words based on their respective POS tags
+    Custom tokenizer that only tokenizes words that possess a specific POS tag
+    ---
+    Args:
+        sentence: A given sentence in string format
+        tags: a list of string that describe which tasks should be tokenized (for nouns, verbs or adjectives) 
+        separator: special character(s) use by tokenizers when splitting words (e.g., `##` for BERT).
+    Returns:
+        A string of the tokenized sentence
     """
-    p = []
-    for token in NLP(premises):
+    tokenized = []
+    for token in NLP(sentence):
       if token.pos_ in tags:
-        components = split_noun_hybrid(token.text)
-        p.extend(components)
-        #for char in token.text:
-          #if char not in encoding.special_tokens:
-            #s.append(char)
+        tokenized.append(f"{separator}{split_word_hybrid(token.text)}")
+      elif token.pos_ == "PUNCT":
+        tokenized.append(token.text)
       else:
-        p.append(token.text)
-    h = []
-    for token in NLP(hypothesis):
-      if token.pos_ in tags:
-        components = split_noun_hybrid(token.text)
-        h.extend(components)
-        #for char in token.text:
-          #if char not in encoding.special_tokens:
-            #s.append(char)
-      else:
-        h.append(token.text)
-    return ' '.join(p), ' '.join(h)
+        tokenized.append(f"{separator}{token.text}")
+    return ' '.join(tokenized)
 
-def noun_tokenizer(premises, hypothesis):
-   return pos_tokenizer(premises, hypothesis, ['NOUN', 'PROPN'])
+def noun_tokenizer(premises_hypothesis, separator):
+   return pos_tokenizer(premises_hypothesis[0], ['NOUN', 'PROPN'], separator), pos_tokenizer(premises_hypothesis[1], ['NOUN', 'PROPN'], separator),
 
-def verb_tokenizer(premises, hypothesis):
-   return pos_tokenizer(premises, hypothesis, ['NOUN', 'PROPN']) 
+def verb_tokenizer(premises_hypothesis, separator):
+   return pos_tokenizer(premises_hypothesis[0], ['VERB'], separator), pos_tokenizer(premises_hypothesis[1], ['VERB'], separator)
 
-premises = "The cat is large. An ant is small."
-hypothesis = "The cat is bigger than the ant."
-print(pos_tokenizer(premises, hypothesis, ['NOUN', 'PROPN']))
-print(pos_tokenizer(premises, hypothesis, ['VERB']))
+def adjective_tokenizer(premises_hypothesis, separator):
+   return pos_tokenizer(premises_hypothesis[0], ['ADJ'], separator), pos_tokenizer(premises_hypothesis[1], ['ADJ'], separator)  
+
+
+premises_hypothesis = ("The cat is large. An ant is small.", "The cat is bigger than the ant.")
+print(noun_tokenizer(premises_hypothesis, "#"))
+print(verb_tokenizer(premises_hypothesis, "#"))
+print(adjective_tokenizer(premises_hypothesis, "#"))
 
