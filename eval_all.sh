897d7ed731e5492c20966ad337b3a548f01f5c02
Tron404 <edi.raul@yahoo.com>
2025-01-29 19:21:59 +0100

    fixed bug in eval bash script

--- eval_all.sh
+++ eval_all.sh
@@ -1,15 +1,15 @@
 tok_methods_pos=("adj" "char" "noun" "verb")
-tok_methods_length=("unigram_tokenizer")
+tok_methods_length=("unigram_tokenizer" "custom_tokenization_word_length" "bigram_tokenizer" "trigram_tokenizer")
 tok_methods_greedy=("greedy_prefix_tokenization" "greedy_suffix_tokenization" "greedy_longest_tokenization")
 
 data_set_paths=(
     "data/multinli_1.0/multinli_1.0_dev_mismatched.jsonl"
-    # "data/snli_1.0/snli_1.0_test.jsonl"
+    "data/snli_1.0/snli_1.0_test.jsonl"
 )
 
-methods_to_loop=$tok_methods_pos
-# methods_to_loop=$tok_methods_greedy
-methods_to_loop=$tok_methods_length
+# methods_to_loop=${tok_methods_pos[@]}
+methods_to_loop=${tok_methods_greedy[@]}
+# methods_to_loop=${tok_methods_length[@]}
 model=roberta
 for dataset in ${data_set_paths[@]}; do
     thread_limit=0
