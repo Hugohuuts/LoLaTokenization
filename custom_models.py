92f7e3a05361ddd0411fcfd72d49819288127c78
JungCesar <julius.bijkerk@icloud.com>
2025-02-01 17:48:16 +0100

    Expanded and cleaned the jupyter notebook

--- custom_models.py
+++ custom_models.py
@@ -4,27 +4,60 @@ from transformers.modeling_outputs import SequenceClassifierOutput
 from transformers import AutoConfig, AutoModelForSequenceClassification, AutoTokenizer
 from transformers import RobertaConfig, BertConfig, PreTrainedModel
 
+
 class CustomModelAbstract(PreTrainedModel, ABC):
     """
     Abstract class serving as a template for custom models.
     """
+
     def __init__(self, model_name: str, **kwargs) -> None:
-        config = AutoConfig.from_pretrained(model_name, output_attentions=True, output_hidden_states=True)
+        config = AutoConfig.from_pretrained(
+            model_name, output_attentions=True, output_hidden_states=True
+        )
         super().__init__(config)
         self._load_model(model_name)
 
     def _load_model(self, model_name: str) -> None:
-        self.model = AutoModelForSequenceClassification.from_pretrained(model_name, config=self.config)
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            model_name, config=self.config
+        )
 
     @abstractmethod
-    def forward(self, input_ids = None, attention_mask = None, token_type_ids = None, position_ids = None, head_mask = None, inputs_embeds = None, labels = None, output_attentions = None, output_hidden_states = None, return_dict = None):
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
         pass
+
 
 class CustomRoberta(CustomModelAbstract):
     config_class = RobertaConfig
 
-    def forward(self, input_ids = None, attention_mask = None, token_type_ids = None, position_ids = None, head_mask = None, inputs_embeds = None, labels = None, output_attentions = None, output_hidden_states = None, return_dict = None):
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = (
+            return_dict if return_dict is not None else self.config.use_return_dict
+        )
 
         outputs = self.model.roberta(
             input_ids,
@@ -52,11 +85,26 @@ class CustomRoberta(CustomModelAbstract):
             attentions=outputs.attentions,
         )
 
+
 class CustomBert(CustomModelAbstract):
     config_class = BertConfig
 
-    def forward(self, input_ids = None, attention_mask = None, token_type_ids = None, position_ids = None, head_mask = None, inputs_embeds = None, labels = None, output_attentions = None, output_hidden_states = None, return_dict = None):
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = (
+            return_dict if return_dict is not None else self.config.use_return_dict
+        )
 
         outputs = self.model.bert(
             input_ids,
@@ -87,11 +135,14 @@ class CustomBert(CustomModelAbstract):
             attentions=outputs.attentions,
         )
 
-def load_custom_class(model_name_or_repo_link: str, device: torch.device=None, **model_args) -> tuple[AutoTokenizer, CustomModelAbstract]:
+
+def load_custom_class(
+    model_name_or_repo_link: str, device: torch.device = None, **model_args
+) -> tuple[AutoTokenizer, CustomModelAbstract]:
     """
     Wrapper method to automatically load (or download) a given custom model.
     ---
-    Args: 
+    Args:
         model_name_or_repo_link: name of the model to be loaded, in the `HuggingFace` format: `<author>/<model_name>`.
         device: on which device to load the model; if not given, automatically detects the best option.
         model_args: additional arguments for model loading, e.g. using only specific hidden layers.
@@ -106,9 +157,12 @@ def load_custom_class(model_name_or_repo_link: str, device: torch.device=None, *
 
     if "roberta" in model_name_or_repo_link.lower():
         class_type = CustomRoberta
-    elif "bert" in model_name_or_repo_link.lower() and "roberta" not in model_name_or_repo_link.lower():
+    elif (
+        "bert" in model_name_or_repo_link.lower()
+        and "roberta" not in model_name_or_repo_link.lower()
+    ):
         class_type = CustomBert
-    
+
     assert class_type != None, "Could not find a class with that name."
 
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_repo_link)
